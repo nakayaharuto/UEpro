eb96e00486a8e7348246eafb816a17033ee52d8f
DESKTOP-GV6LMIL\GA3 <h1202419010@hiratagakuen.onmicrosoft.com>
2026-04-15 16:35:05 +0900

    コードの変更

--- Source/UEProject/UEProject.Build.cs
+++ Source/UEProject/UEProject.Build.cs
@@ -8,7 +8,15 @@ public class UEProject : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core", 
+			"CoreUObject", 
+			"Engine",
+			"InputCore", 
+			"EnhancedInput", 
+			"Niagara",
+			"CableComponent",
+		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
